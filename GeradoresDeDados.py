b78af3fbc1946a2a390c521ac09cfd1f6c2aea78
Artur Galeno <artur_galenomuniz@yahoo.com.br>
2012-02-17 23:52:55 -0300

    Inserts de primary keys não repetidas implementado. Aviso de inconsistencia em dupla referencia começando a ser implementado. Alguns tipos de dados implementados no metodo de geradores de dados

--- GeradoresDeDados.py
+++ GeradoresDeDados.py
@@ -16,7 +16,7 @@ class GeradoresDeDados(object):
         z = ''.join(random.choice(string.ascii_uppercase + string.digits) for a in range(0,int(tam))) #@UnusedVariable
         return z
     
-    def gerarDate(self,de=2000,ate=2011):
+    def gerarDate(self,de=2000,ate=2012):
         ''' Gera um campo Date. Utiliza como entrada os valores inteiros "de" e "ate". "de" eh o ano o qual você deseja que os valores
         aleatórios partam, e "ate" é o ano limite dos valores aleatórios. "ate" tem que ser maior que "de", e ambos devem ser
         valores válidos ao SGBD'''
@@ -44,4 +44,49 @@ class GeradoresDeDados(object):
         parteDecimal = ''.join(aux for i in range (0,tmp_parteDecimal))  #@UnusedVariable  
         z = str(random.randint(0, int(parteInteira)))
         z = z+'.'+str(random.randint(0, int(parteDecimal)))
+        return z
+    def gerarTinyInt(self):
+        return self.gerarInt(de=-128,ate=127)
+    def gerarBit(self):
+        return self.gerarInt(de=0, ate=1)
+    def gerarSmallInt(self):
+        return self.gerarInt(de=-32768, ate=32767)
+    def gerarMediumInt(self):
+        return self.gerarInt(de=-8388608, ate=8388607)
+    def gerarBigInt(self):
+        return self.gerarInt(de=-9223372036854775808, ate=-9223372036854775807)
+    def gerarFloat(self,row):
+        tam = row[1][8:]
+        tam = tam[:-1]
+        tam = tam.split(',')
+        tmp_parteDecimal = (int(tam[1]))
+        tmp_parteInteira = int(tam[0]) - tmp_parteDecimal
+        aux = '9'
+        parteInteira = ''.join(aux for i in range(0,tmp_parteInteira)) #@UnusedVariable
+        parteDecimal = ''.join(aux for i in range (0,tmp_parteDecimal))  #@UnusedVariable  
+        z = str(random.randint(0, int(parteInteira)))
+        z = z+'.'+str(random.randint(0, int(parteDecimal)))
+        return z
+    def gerarDouble(self,row):
+        tam = row[1][8:]
+        tam = tam[:-1]
+        tam = tam.split(',')
+        tmp_parteDecimal = (int(tam[1]))
+        tmp_parteInteira = int(tam[0]) - tmp_parteDecimal
+        aux = '9'
+        parteInteira = ''.join(aux for i in range(0,tmp_parteInteira)) #@UnusedVariable
+        parteDecimal = ''.join(aux for i in range (0,tmp_parteDecimal))  #@UnusedVariable  
+        z = str(random.randint(0, int(parteInteira)))
+        z = z+'.'+str(random.randint(0, int(parteDecimal)))
+        return z
+    def gerarDateTime(self,de=2000,ate=2012):
+        z = ('' + str(random.randint(de,ate)) + '-' + str(random.randint(01, 12)) + '-' + str(random.randint(01, 24)) + '')
+        z = z + ('' + str(random.randint(0,23)) + '-' + str(random.randint(0, 59)) + '-' + str(random.randint(0, 59)) + '')
+        return z
+    def gerarTimeStamp(self,de=2000,ate=2012):
+        z = ('' + str(random.randint(de,ate)) + '-' + str(random.randint(01, 12)) + '-' + str(random.randint(01, 24)) + '')
+        z = z + ('' + str(random.randint(0,23)) + '-' + str(random.randint(0, 59)) + '-' + str(random.randint(0, 59)) + '')
+        return z
+    def gerarYear(self,de=2000,ate=2012):
+        z = ('' + str(random.randint(de,ate)))
         return z
--- InsertEmTodoBanco.py
+++ InsertEmTodoBanco.py
@@ -22,13 +22,25 @@ class InsertEmTodoBanco(object):
         self.db = db
         self.criarInsert()
     
-    def relacionamentosDe(self,tabela):
+    def relacionamentosDe(self,tabela,log=False):
         bd = BD(self.host,self.user,self.passwd,'information_schema')
         rows = bd.retornarLinhas("SELECT * FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_SCHEMA = \'%s\' AND TABLE_NAME = \'%s\' AND REFERENCED_TABLE_NAME IS NOT NULL;" %(self.db,tabela))
+        tmp_listaDeRelacionamentos = []
         listaDeRelacionamentos = []
         for row in rows:
             aux = (row[4],row[5],row[6],row[9],row[10],row[11])
-            listaDeRelacionamentos.append(aux)
+            tmp_listaDeRelacionamentos.append(aux)
+        for relacao in tmp_listaDeRelacionamentos:
+            num = tmp_listaDeRelacionamentos.count(relacao)
+            if num != 1 and log == True and (relacao not in listaDeRelacionamentos):
+                print "----CUIDADO!!! A relação na ----"
+                print "Tabela: %s Coluna: %s" %(relacao[1],relacao[2])
+                print "Com estrangeirismo na tabela: %s Coluna: %s" %(relacao[4],relacao[5])
+                print "Aparece %s veses. Verifique se esse tipo de relação é mesmo necessário." %(str(num))
+                print "Pois há referêcias ambíguas com 'CONSTRAINTS' diferentes."
+                print ""
+            if relacao not in listaDeRelacionamentos:
+                listaDeRelacionamentos.append(relacao)
         return listaDeRelacionamentos
     
     def obterColunasDe(self, tabela):
@@ -59,42 +71,91 @@ class InsertEmTodoBanco(object):
         
         for tabela in tabelas:
             print "Gerando Insert para Tabela: "+tabela
+
+            bd = BD(self.host,self.user,self.passwd,self.db)
+            colunas = self.obterColunasDe(tabela)
+            colunasChavesPrimarias = []
+            chavesEstrangeiras = self.relacionamentosDe(tabela,log=True)
             
-            rows = self.relacionamentosDe(tabela)
+            for coluna in colunas:
+                if coluna[3] == 'PRI':
+                    colunasChavesPrimarias.append(coluna[0])
+                    bd = BD(self.host,self.user,self.passwd,self.db)
+                    chavesPrimarias = bd.retornarLinhas("SELECT "+coluna[0]+" FROM "+tabela)
+                    controle = False
+                    while controle == False:
+                        if coluna[1][0:7] == 'varchar':
+                            gerador = GeradoresDeDados()
+                            valor = gerador.gerarVarchar(coluna)
+                            if (valor,) not in chavesPrimarias:
+                                values1.append(coluna[0])
+                                values.append(valor)
+                                controle = True
+                                                        
+                        if coluna[1][0:4] == 'date':
+                            gerador = GeradoresDeDados()
+                            valor = gerador.gerarDate()
+                            if (valor,) not in chavesPrimarias:
+                                values1.append(coluna[0])
+                                values.append(valor)
+                                controle = True
+                            
+                        if coluna[1][0:3] == 'int':
+                            gerador = GeradoresDeDados()
+                            valor = gerador.gerarInt()
+                            if (long(valor),) not in chavesPrimarias:
+                                values1.append(coluna[0])
+                                values.append(valor)
+                                controle = True
+                            
+                        if coluna[1][0:4] == 'char':
+                            gerador = GeradoresDeDados()
+                            valor = gerador.gerarChar(coluna)
+                            if (valor,) not in chavesPrimarias:
+                                values1.append(coluna[0])
+                                values.append(valor)
+                                controle = True
+                            
+                        if coluna[1][0:7] == 'decimal':
+                            gerador = GeradoresDeDados()
+                            valor = gerador.gerarDecimal(coluna)
+                            if (valor,) not in chavesPrimarias:
+                                values1.append(coluna[0])
+                                values.append(valor)
+                                controle = True
             
-            if rows != []:
-                for relacao in rows:
+            if chavesEstrangeiras != []:
+                for relacao in chavesEstrangeiras:
                     bd = BD(self.host,self.user,self.passwd,self.db) 
                     rows1 = bd.retornarLinhas('SELECT '+relacao[5]+' FROM '+relacao[4])
                     aux = random.randint(0,len(rows1)-1)
                     choosed = rows1[aux-1][0]
                     values1.append(relacao[2])
                     values.append(str(choosed))
-                                                
-            colunas = self.obterColunasDe(tabela)
             
             for row in colunas:                                
                 if row[0] not in [relacao[2] for relacao in self.relacionamentosDe(tabela)]:
-                    values1.append(row[0])
-                    if row[1][0:7] == 'varchar':
-                        gerador = GeradoresDeDados()
-                        values.append(gerador.gerarVarchar(row))
-                        
-                    if row[1][0:4] == 'date':
-                        gerador = GeradoresDeDados()
-                        values.append(gerador.gerarDate())
-                        
-                    if row[1][0:3] == 'int':
-                        gerador = GeradoresDeDados()
-                        values.append(gerador.gerarInt())
-                        ''
-                    if row[1][0:4] == 'char':
-                        gerador = GeradoresDeDados()
-                        values.append(gerador.gerarChar(row))
-                        
-                    if row[1][0:7] == 'decimal':
-                        gerador = GeradoresDeDados()
-                        values.append(gerador.gerarDecimal(row))
+                    if row[0] not in colunasChavesPrimarias:
+                        values1.append(row[0])
+                        if row[1][0:7] == 'varchar':
+                            gerador = GeradoresDeDados()
+                            values.append(gerador.gerarVarchar(row))
+                            
+                        if row[1][0:4] == 'date':
+                            gerador = GeradoresDeDados()
+                            values.append(gerador.gerarDate())
+                            
+                        if row[1][0:3] == 'int':
+                            gerador = GeradoresDeDados()
+                            values.append(gerador.gerarInt())
+                            ''
+                        if row[1][0:4] == 'char':
+                            gerador = GeradoresDeDados()
+                            values.append(gerador.gerarChar(row))
+                            
+                        if row[1][0:7] == 'decimal':
+                            gerador = GeradoresDeDados()
+                            values.append(gerador.gerarDecimal(row))
             
             string1 = ''
             c = '\''       
