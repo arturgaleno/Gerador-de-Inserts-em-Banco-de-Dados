0f42bfaa3b1e8adcc650df9aaaf183c01b282f9b
Artur Galeno <artur_galenomuniz@yahoo.com.br>
2012-02-21 15:19:50 -0300

    Simplificação de InsertEmTodoBanco

--- Main.py
+++ Main.py
@@ -3,12 +3,13 @@ Created on 07/02/2012
 
 @author: Artur Galeno Muniz
 '''
-#import InsertEmTodoBanco
+import InsertEmTodoBanco
 #from AssistenteDeDelecao import * #@UnusedWildImport
 #import InsertEmTabela
 
 if __name__ == '__main__':
     #for a in range(0,100):
-        #InsertEmTodoBanco.InsertEmTodoBanco(host='',user='',passwd='',db='')
-        #assistente = AssistenteDeDelecao(host='',user='',passwd='',db='')
+        InsertEmTodoBanco.InsertEmTodoBanco(host='localhost',user='root',passwd='1n5p1r0n123',db='Tutor')
+        #InsertEmTabela.InsertEmTabela(host='',user='',passwd='',db='')
+        #assistente = AssistenteDeDelecao(host='localhost',user='root',passwd='1n5p1r0n123',db='DIGITEC')
         #assistente.limparBanco()
